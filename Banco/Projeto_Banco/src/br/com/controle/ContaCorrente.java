cdfeec802d48faa50b4e7f872df77203d6e82106
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-28 12:22:52 -0300

    Criaçãod e projeto e classes

---
+++ Banco/Projeto_Banco/src/br/com/controle/ContaCorrente.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class ContaCorrente {
+    private double saldo;
+    private double deposito;
+    private double saque;
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getDeposito() {
+        deposito -= saldo;
+        return deposito;
+    }
+
+    public void setDeposito(double deposito) {
+        this.deposito = deposito;
+    }
+
+    public double getSaque() {
+        return saque;
+    }
+
+    public void setSaque(double saque) {
+        this.saque = saque;
+    }
+    
+    
+}
