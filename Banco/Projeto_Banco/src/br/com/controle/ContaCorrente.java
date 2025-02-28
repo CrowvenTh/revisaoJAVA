3d0b0eec00a49650a8a1a49e18515eed4017b95c
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-28 13:08:42 -0300

    conclusão de projeto banco I

--- Banco/Projeto_Banco/src/br/com/controle/ContaCorrente.java
+++ Banco/Projeto_Banco/src/br/com/controle/ContaCorrente.java
@@ -55,30 +55,39 @@ public class ContaCorrente {
         System.out.println("Insira o valor do depoósito");
         deposito = scanner.nextDouble();
         saldo += deposito;
-        System.out.println("Depósito de R$" + this.deposito + " realizado com sucesso!\nSaldo atual: R$" + saldo);
+        System.out.println("Depósito de R$" + this.deposito + " realizado com sucesso!\nSaldo atual: R$" + saldo+"\n");
     }
 
     public void Sacar() {
         System.out.println("Insira o valor do saque");
         saque = scanner.nextDouble();
         if (saque > saldo) {
-            System.out.println("Saldo insuficiente!");
+            System.out.println("Saldo insuficiente!\n");
         } else {
             saldo -= saque;
-            System.out.println("Saque de R$" + saque + "realizado com sucesso!\nSaldo atual: R$" + saldo);
+            System.out.println("Saque de R$" + saque + "realizado com sucesso!\nSaldo atual: R$" + saldo+"\n");
         }
     }
 
     public void VerificarSaldo() {
-        System.out.println("Saldo atual: R$" + saldo);
+        System.out.println("Saldo atual: R$" + saldo+"\n");
     }
 
     public void Operacao() {
-        System.out.println("Selecione uma operação");
+        System.out.println("| Selecione uma operação |");
+        System.out.println("---| 1 = Depósito    |---");
+        System.out.println("---| 2 = Saque       |---");
+        System.out.println("---| 3 = Saldo       |---");
+        System.out.println("---| 0 = Encerrar    |---");
+        System.out.println("-------------------------");
+        
         opcao = scanner.nextInt();
     }
 
     public void Menu() {
+        System.out.println("\n-------------------------");
+        System.out.println("-----| BANCO JAVA |------");
+        System.out.println("-------------------------");
         Operacao();
         while (opcao != 0) {
             switch (opcao) {
